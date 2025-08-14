3a872da1e079603946b2c0e752f70033eb959b95
Milad.Msr <Milad.Msrii@gmail.com>
2025-08-14 22:49:55 +0330

    create navbar menu in mobile

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ window.addEventListener('load', () => {
   document.body.classList.add('loaded');
 });
 
+
+
+
+const navbarMenu = document.querySelector('[data-navbar]');
+const closeBtn = document.querySelector('[data-close-btn]');
+const openBtn = document.querySelector('[data-open-nav-btn]');
+
+
+
+openBtn.addEventListener('click', () => {
+  navbarMenu.classList.remove('hide')
+})
+
+
+closeBtn.addEventListener('click', () => {
+  navbarMenu.classList.add('hide')
+})
+
+
